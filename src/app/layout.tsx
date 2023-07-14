ef59d047011ed1bd57322527eb0a86dfb3786227
gabewolford <gabewolford@gmail.com>
2023-07-14 12:54:01 -0700

    tweaked layout margins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
-import SizzleReel from './components/SizzleReel/SizzleReel'
+import SizzleReel from './components/SizzleReel'
+import Footer from './components/Footer'
 
 export const metadata: Metadata = {
   title: 'Small Hound',
@@ -16,11 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='ml-4 md:ml-11 mr-4 my-4 md:my-8'>
+      <body className='mt-4 md:mt-8'>
         <Navbar />
         {children}
         <SizzleReel />
         <Analytics />
+        <Footer />
       </body>
     </html>
   )
