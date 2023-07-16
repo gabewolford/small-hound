bb1c17ce2f700d39ed6bc6215fbf1866ba7824f9
gabewolford <gabewolford@gmail.com>
2023-07-16 10:14:23 -0700

    time state functioning

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,43 +8,43 @@ export default function Navbar() {
   
     useEffect(() => {
         const updateTime = (): void => {
-        const currentTime: Date = new Date();
-        const options: Intl.DateTimeFormatOptions = {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-            timeZoneName: 'short',
+            const currentTime: Date = new Date();
+            const options: Intl.DateTimeFormatOptions = {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+                timeZoneName: 'short',
+            };
+            const newFormattedTime: string = currentTime.toLocaleString(undefined, options);
+            setFormattedTime(newFormattedTime);
         };
-        const newFormattedTime: string = currentTime.toLocaleString(undefined, options);
-        setFormattedTime(newFormattedTime);
+
+        updateTime(); 
+
+        const interval: NodeJS.Timeout = setInterval(updateTime, 60000);
+
+        return () => {
+            clearInterval(interval);
         };
-
-    updateTime(); 
-
-    const interval: NodeJS.Timeout = setInterval(updateTime, 60000);
-
-    return () => {
-        clearInterval(interval);
-    };
     }, []);
 
     return (
-            <nav className="flex justify-between">
-                <div>
-                    <Link href="/" className="text-[32px] lg:text-[84px] leading-none bold">Small Hound</Link>
-                </div>
+        <nav className="flex justify-between">
+            <div>
+                <Link href="/" className="text-[32px] lg:text-[84px] leading-none bold">Small Hound</Link>
+            </div>
 
-                <div className="md:mr-36">
-                    <h5 className="text-sm md:text-base">Portland, OR</h5>
-                </div>
-        
-                <div className="hidden md:inline-block md:mr-12">
-                    <h5>{formattedTime}</h5>
-                </div>
+            <div className="md:mr-36">
+                <h5 className="text-sm md:text-base">Portland, OR</h5>
+            </div>
 
-                <div className="hidden md:inline-block mr-4">
-                    <Link href="mailto:hi@smallhound.co" className="text-white bold bg-red p-4 rounded-full">Get in touch</Link>
-                </div>
-            </nav>
+            <div className="hidden md:inline-block md:mr-12">
+                <h5>{formattedTime}</h5>
+            </div>
+
+            <div className="hidden md:inline-block mr-4">
+                <Link href="mailto:hi@smallhound.co" className="text-white bold bg-red p-4 rounded-full">Get in touch</Link>
+            </div>
+        </nav>
     )
   }
