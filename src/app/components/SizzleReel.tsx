581b85d24657f3a6b1bebf2bd5407a1c84d52595
gabewolford <gabewolford@gmail.com>
2023-07-16 20:40:49 -0700

    sizzle reel placeholder

--- src/app/components/SizzleReel.tsx
+++ src/app/components/SizzleReel.tsx
@@ -1,7 +1,7 @@
 export default function SizzleReel() {
     return(
-        <section className="mb-16">
-            sizzle reel placeholder
+        <section className="mb-16 h-[760px]">
+            <img src="/images/placeholder.svg"/>
         </section>
     )
 }
