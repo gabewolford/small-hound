ec7012246c479b75fbc924d9eb442460c50e15c1
gabewolford <gabewolford@gmail.com>
2023-07-27 17:23:29 -0700

    removed comment

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,6 @@ import Link from "next/link"
 
 export default function Footer() {
     return(
-        // <footer className="flex flex-col md:flex-row justify-between text-sm mt-11 pb-4">
         <footer className="flex flex-col md:grid md:grid-cols-12 md:mt-11 pb-4 text-sm">
             <div className="col-span-3">
                 <h5>© 2023 Small Hound</h5>
@@ -15,8 +14,6 @@ export default function Footer() {
             <div className="col-span-3 md:ml-12">
                 <h5>Full site coming soon.</h5>
             </div>
-
-
         </footer>
     )
 }
