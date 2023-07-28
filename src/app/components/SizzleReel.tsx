32b2695c4fdf9ff7e0ffa918e2fa82295b1322a8
gabewolford <gabewolford@gmail.com>
2023-07-27 17:24:02 -0700

    added placeholder gif for sizzle reel

--- src/app/components/SizzleReel.tsx
+++ src/app/components/SizzleReel.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image"
+
 export default function SizzleReel() {
     return(
-        <section className="mb-16 h-[760px]">
-            <img src="/images/placeholder.svg"/>
+        <section className="mx-auto my-20">
+            <figure>
+                <Image
+                    src="/images/typing-cat-omar.gif"
+                    height={1000}
+                    width={1000}
+                    alt="typing cat gif"
+                >
+                </Image>
+                <figcaption className="mt-4">
+                    <p>Cooking up a sizzle reel for you.</p>
+                    <p>Check back soon...</p>
+                </figcaption>
+            </figure>
         </section>
     )
 }
