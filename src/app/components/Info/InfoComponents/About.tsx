2652781b6fda530f5f10746746ec8696730fa6fd
gabewolford <gabewolford@gmail.com>
2023-10-10 21:38:27 -0700

    fixed typo

--- src/app/components/Info/InfoComponents/About.tsx
+++ src/app/components/Info/InfoComponents/About.tsx
@@ -1,10 +1,15 @@
-import { apercuMedium } from "@/app/styles/fonts"
+import { apercuMedium } from "@/app/styles/fonts";
 
 export default function About() {
-    return (
-        <div className="lg:col-span-3 flex flex-col md:px-8 gap-4 border-red md:border-l md:border-t-0 border-t pt-[18px] md:pt-0 self-stretch flex100">
-            <h2 className={`text-[24px] leading-[38.40px] ${apercuMedium.className}`}>Indepedent digital product&nbsp;&&nbsp;web&nbsp;studio.</h2>
-            <p className="leading-6">We partner with our clients to craft digital experiences that connect people to their products and services.</p>
-        </div>
-    )
+  return (
+    <div className="lg:col-span-3 flex flex-col md:px-8 gap-4 border-red md:border-l md:border-t-0 border-t pt-[18px] md:pt-0 self-stretch flex100">
+      <h2 className={`text-[24px] leading-[38.40px] ${apercuMedium.className}`}>
+        Independent digital product&nbsp;&&nbsp;web&nbsp;studio.
+      </h2>
+      <p className="leading-6">
+        We partner with our clients to craft digital experiences that connect
+        people to their products and services.
+      </p>
+    </div>
+  );
 }
